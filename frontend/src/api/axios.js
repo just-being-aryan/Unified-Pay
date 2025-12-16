2bd5e547e323f851da62a591ab6a6be4e5036d4d
just-being-aryan <aryansachdevaa@gmail.com>
2025-12-16 10:09:02 +0530

    Removed ngrok skip browser warning from axios.js for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,7 +7,7 @@ const api = axios.create({
   baseURL: API_BASE_URL,
   headers: { 
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true"  
+    // "ngrok-skip-browser-warning": "true"  
   },
   withCredentials: true,
 });
